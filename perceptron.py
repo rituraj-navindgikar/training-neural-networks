a34a0b515f540303aab7ef5fc4ff12cfb9fd7d76
Rituraj Navindgikar <riturajn1200@gmail.com>
2025-01-15 00:18:50 -0500

    adding back propagation

--- perceptron.py
+++ perceptron.py
@@ -34,29 +34,36 @@ class MultiLayerPerceptron:
     '''
     A Multilayer perceptron class that uses perceptron class
     '''
-    def __init__(self, layers, bias = 1.0):
+    def __init__(self, layers, bias = 1.0, eta = 0.5):
         self.layers = np.array(layers,dtype=object)
         self.bias = bias
         self.network  = [] # list of neurons
         self.values = [] # list of values
+        self.d = []
+        self.eta = eta
 
         for i in range(len(self.layers)):
             self.values.append([])
             self.network.append([])
+            self.d.append([])
             self.values[i] = [0.0 for j in range(self.layers[i])]
+            self.d[i] = [0.0 for j in range(self.layers[i])]
+
             if i > 0: # network[0] is input later, it has no neurons
                 for _ in range(self.layers[i]):
                     self.network[i].append(Perceptron(inputs=self.layers[i-1], bias=self.bias))
 
         self.network = np.array([np.array(x) for x in self.network], dtype=object)
         self.values = np.array([np.array(x) for x in self.values], dtype=object)
+        self.d = np.array([np.array(x) for x in self.d], dtype=object)
+
 
     def set_weights(self, w_init):
         for i in range(len(w_init)):
             for j  in range(len(w_init[i])):
                 self.network[i+1][j].set_weights(w_init[i][j])
 
-    def printWeights(self):
+    def print_weights(self):
         print()
         for i in range(1, len(self.network)):
             for j in range(self.layers[i]):
@@ -72,4 +79,35 @@ class MultiLayerPerceptron:
                 self.values[i][j] = self.network[i][j].run(self.values[i-1])
 
         return self.values[-1]
+    
+    def back_propagation(self, x, y):
+        x = np.array(x, dtype=object)
+        y = np.array(y, dtype=object)
+        
+        outputs = self.run(x)
+
+        error = (y - outputs)
+
+        mse = sum(error ** 2) / self.layers[-1]
+
+        self.d[-1] = outputs * (1 - outputs) * (error)
+
+        for i in reversed(range(1, len(self.network) - 1)):
+            for h in range(len(self.network[i])):
+                fwd_error = 0.0
+                for k in range(self.layers[i+1]):
+                    fwd_error += self.network[i+1][k].weights[h] * self.d[i+1][k]
+                self.d[i][h] = self.values[i][h] * (1 - self.values[i][h]) * fwd_error
+
+
+        for i in range(1, len(self.network)):
+            for j in range(self.layers[i]):
+                for k in range(self.layers[i-1] + 1):
+                    if k == self.layers[i-1]:
+                        delta = self.eta * self.d[i][j] * self.bias
+                    else:
+                        delta = self.eta * self.d[i][j] * self.values[i-1][k]
+                    self.network[i][j].weights[k] += delta
+        return mse
+
 
